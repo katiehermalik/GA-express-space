da5fa1472643c1d8e794c8bceadb218ae9e97b29
Reuben Ayres <ra@sent.com>
2018-10-08 12:04:37 -0500

    one more change

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 // * The app will need routes for index and show
 // * The app will need views for index and show
 //
+// * MAIN GOAL:
 // * User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page
 //
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
