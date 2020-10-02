1ff22698169b69613f889c32c576025a06cc1f71
khermalik <khermalik@gmail.com>
2020-10-01 20:06:39 -0700

    set up file structures and installed dependencies

---
+++ controllers/missionsController.js
@@ -0,0 +1,8 @@
+// INDEX Route
+// send data to 'missions/index.ejs' view
+// the view should display just the names of each mission
+// display the mission names as <li> in a <ul> with the class name "missions"
+
+// SHOW Route
+// send data to 'missions/show.ejs' view
+// the view should display all the data for a single mission
--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-// DEPENDENCIES
-const express = require('express');
-const app = express();
-
-// run `npm install` to install dependencies in package.json
-
 // * Your mission is to complete the app
 // * The app will need routes for index and show
 // * The app will need views for index and show
@@ -14,64 +8,15 @@ const app = express();
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
 // * Bonus/Hungry for More: add static css to style the pages (google how)
 
-// NOTES:
-// ejs has not been installed
-// views folder has not been created
-// views/missions folder has not been created
+// DEPENDENCIES
+const express = require('express');
+const app = express();
 
 // PORT
 const port = 3000;
 
-// DATA - put into marsMissions.js file inside of a models folder, for module.exports
-// remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
-
-// INDEX Route
-// send data to 'missions/index.ejs' view
-// the view should display just the names of each mission
-// display the mission names as <li> in a <ul> with the class name "missions"
-
-// SHOW Route
-// send data to 'missions/show.ejs' view
-// the view should display all the data for a single mission
-
-
+// Set View Engine
+app.set('view engine', 'ejs');
 
 // LISTENER
 app.listen(port, function() {
